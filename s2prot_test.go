106ab4cc8593f9e3d1885b1015a5a2af4061cff3
Andras Belicza <iczaaa@gmail.com>
2016-10-20 09:44:34 +0200

    Added support for 3.7 reps.

--- s2prot_test.go
+++ s2prot_test.go
@@ -1,29 +1,23 @@
 package s2prot
 
 import (
-	"github.com/icza/s2prot/build"
 	"testing"
+
+	"github.com/icza/s2prot/build"
 )
 
 func TestGetProtocol(t *testing.T) {
-	cases := []struct {
-		baseBuild int
-	}{
-		{15405},
-		{34835},
-	}
-
-	for _, c := range cases {
-		p := GetProtocol(c.baseBuild)
+	for baseBuild, _ := range build.Builds {
+		p := GetProtocol(baseBuild)
 		if p == nil {
-			t.Errorf("Parsing protocol %d failed!", c.baseBuild)
+			t.Errorf("Parsing protocol %d failed!", baseBuild)
 		}
 	}
 }
 
 func BenchmarkParseProtocol(b *testing.B) {
 	//baseBuild := 15405
-	baseBuild := 34835
+	baseBuild := 47185
 
 	for i := 0; i < b.N; i++ {
 		parseProtocol(build.Builds[baseBuild], baseBuild)
