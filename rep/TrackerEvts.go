a9afce06bc8838cd3a5d0601547d9864467f515b
Andras Belicza <iczaaa@gmail.com>
2017-07-24 13:48:02 +0200

    Added and filled fields to PlayerDesc: SlotID, UserID, SQ, SupplyCappedPercent.

--- rep/TrackerEvts.go
+++ rep/TrackerEvts.go
@@ -15,8 +15,12 @@ import (
 const (
 	// TrackerEvtIDPlayerStats is the ID of the Player Stats tracker event
 	TrackerEvtIDPlayerStats = 0
+
 	// TrackerEvtIDUnitBorn is the ID of the Unit Born tracker event
 	TrackerEvtIDUnitBorn = 1
+
+	// TrackerEvtIDPlayerSetup is the ID of the Player Setup tracker event
+	TrackerEvtIDPlayerSetup = 9
 )
 
 // TrackerEvts contains tracker events and some metrics and data calculated from them.
@@ -33,12 +37,24 @@ type PlayerDesc struct {
 	// PlayerID is the ID of the player this PlayerDesc belongs to.
 	PlayerID int64
 
+	// SlotID is the slot ID of the player
+	SlotID int64
+
+	// UserID is the user ID of the player
+	UserID int64
+
 	// Start location of the player
 	StartLocX, StartLocY int64
 
 	// StartDir is the start direction of the player, expressed in clock,
 	// e.g. 1 o'clock, 3 o'clock etcc, in range of 1..12
 	StartDir int
+
+	// SQ (Spending Quotient) of the player
+	SQ int
+
+	// SupplyCappedPercent is the supply-capped percent of the player
+	SupplyCappedPercent int
 }
 
 // init initializes / preprocesses the tracker events.
@@ -46,14 +62,36 @@ func (t *TrackerEvts) init(rep *Rep) {
 	pidPlayerDescMap := make(map[int64]*PlayerDesc)
 	t.PIDPlayerDescMap = pidPlayerDescMap
 
-	getPD := func(pid int64) *PlayerDesc {
+	// stats per player
+	type stats struct {
+		samples   int64 // stats samples count
+		unspents  int64 // Unspent resources
+		incomes   int64 // Resource income
+		supCapped int64 // supply capped
+	}
+
+	pidStats := make(map[int64]*stats)
+
+	// first read Player setup events:
+	for _, e := range t.Evts {
+		if e.Loop() > 0 {
+			break
+		}
+		if e.Int("ID") != TrackerEvtIDPlayerSetup {
+			continue
+		}
+		pid := e.Int("playerId")
 		pd := pidPlayerDescMap[pid]
 		if pd == nil {
 			pd = &PlayerDesc{PlayerID: pid}
 			pidPlayerDescMap[pid] = pd
+			pidStats[pid] = &stats{}
 		}
-		return pd
+		pd.SlotID = e.Int("slotId")
+		pd.UserID = e.Int("userId")
 	}
+
+	// Read start locations and player stats
 
 	cx := rep.InitData.GameDescription.MapSizeX()
 	cy := rep.InitData.GameDescription.MapSizeY()
@@ -62,16 +100,38 @@ func (t *TrackerEvts) init(rep *Rep) {
 		eid := e.Int("ID")
 		if e.Loop() == 0 && eid == TrackerEvtIDUnitBorn {
 			if isMainBuilding(e.Stringv("unitTypeName")) {
-				pd := getPD(e.Int("controlPlayerId"))
-				pd.StartLocX = e.Int("x")
-				pd.StartLocX = e.Int("x")
-				pd.StartDir = angleToClock(math.Atan2(float64(pd.StartLocY-cy), float64(pd.StartLocX-cx)))
+				pd := pidPlayerDescMap[e.Int("controlPlayerId")]
+				if pd != nil {
+					pd.StartLocX = e.Int("x")
+					pd.StartLocY = e.Int("y")
+					pd.StartDir = angleToClock(math.Atan2(float64(pd.StartLocY-cy), float64(pd.StartLocX-cx)))
+				}
 			}
 		}
 
-		if eid == TrackerEvtIDPlayerStats {
-			// TODO
+		if eid != TrackerEvtIDPlayerStats {
+			pid := e.Int("playerId")
+			st := pidStats[pid]
+			if st != nil {
+				ss := e.Structv("stats")
+				st.samples++
+				st.unspents += ss.Int("scoreValueMineralsCurrent") + ss.Int("scoreValueVespeneCurrent")
+				st.incomes += ss.Int("scoreValueMineralsCollectionRate") + ss.Int("scoreValueVespeneCollectionRate")
+				if ss.Int("scoreValueFoodUsed") >= ss.Int("scoreValueFoodMade") {
+					st.supCapped++
+				}
+			}
 		}
+	}
+
+	// Finish SQ and supply-capped calculations
+	for pid, pd := range pidPlayerDescMap {
+		st := pidStats[pid]
+		if st == nil || st.samples == 0 {
+			continue
+		}
+		pd.SQ = calcSQ(st.unspents/st.samples, st.incomes/st.samples)
+		pd.SupplyCappedPercent = int(st.supCapped * 100 / st.samples)
 	}
 }
 
@@ -113,4 +173,17 @@ func angleToClock(angle float64) int {
 		return 12
 	}
 	return hour
+}
+
+// calcSQ calculates the SQ (Spending Quotient).
+//
+// Algorithm:
+// SQ = 35 * ( 0.00137 * I - ln( U ) ) + 240
+// Where U is the average unspent resources (Resources Current; including minerals and vespene)
+// and I is the average income (Resource Colleciton Rate; including minerals and vespene);
+// and samples are taken up to the loop of the last cmd game event of the user.
+//
+// Source: Do you macro like a pro? http://www.teamliquid.net/forum/viewmessage.php?topic_id=266019
+func calcSQ(unspentResources, income int64) int {
+	return int(35*(0.00137*float64(income)-math.Log(float64(unspentResources))) + 240 + 0.5)
 }
